8fe8ff26c0dca517f7b380bed2b691cfd7a06c3b
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-21 12:32:30 +0600

    Method Find Check Test

--- PYTHON/Method.py
+++ PYTHON/Method.py
@@ -1,4 +1,6 @@
 name = "Sourav Chandra Roy"
 name_array = name[:6]
-print(name_array.upper())
-print(len(name_array))
+print("UpperCase: "+name_array.upper())
+print("Array Length: ", len(name_array))
+print('Index Number: ', name_array.find('a'))
+print('In String: ', 'Sourav' in name)
