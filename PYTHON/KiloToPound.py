f5838f5521d99d3bf0091b038fa60d5d80c0872f
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-21 13:16:59 +0600

    While Loop Is Done

--- PYTHON/KiloToPound.py
+++ PYTHON/KiloToPound.py
@@ -2,9 +2,9 @@ weight = int(input("Enter Inout: "))
 unit = input('(L)bs or (K)g : ')
 if unit.upper() =="L":
     converted = weight * 0.45
-    print(f"You are {converted} Kilos")
+    print(weight, f"Pound from {converted} Kilos")
 elif unit.upper() == "K":
-    converted = weight // 0.45
-    print(f"You Are {converted} Pounds")
+    converted = weight / 0.45
+    print(weight, f"Kilo from {converted} Pounds")
 else:
     print("Enter Right Value")
