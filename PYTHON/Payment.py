686b549de1f4b74be609f6baf5508344b24df812
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-21 10:43:54 +0600

    Payment Update

--- PYTHON/Payment.py
+++ PYTHON/Payment.py
@@ -1,13 +1,13 @@
 # import math
-price = 10
+price = 1000
 has_good_credit = True
 
 if has_good_credit:
     down_payment = price * 0.1
 else:
     down_payment = price * 0.2
-# print(f'Down Payment: {down_payment}')
+print(f'Down Payment: {down_payment}')
 # print('Down Payment: ', down_payment)
 # result = price ** 2
-result = pow(price, 4)
-print('Multiply ', result)
+# result = pow(price, 4)
+# print('Power Result ', result)
