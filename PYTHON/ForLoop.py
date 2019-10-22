4d1d70857c4c1a844d8a6d6d2a4f44835740c901
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-22 13:28:58 +0600

    Total Price With For Loop'

---
+++ PYTHON/ForLoop.py
@@ -0,0 +1,15 @@
+# for item in "Python":
+# for item in ["Sourav", "Chandra", "Roy","OK-DONE"]:
+# for item in range(10):
+# Print 5 7 9
+# for item in range(5, 10, 2):
+# for item in range(5, 10, 2):
+#     print(item)
+
+prices = [50, 60, 80]
+total = 0
+for price in prices:
+    total += price
+    # print("Item Price", total)
+print("**************************")
+print(f"Total Price = {total}")
