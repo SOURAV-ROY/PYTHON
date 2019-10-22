206dd4cbafed71f6f0aed3d09eed1683681ffbc0
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-22 14:39:50 +0600

    All Item Print From Matrix One By One

--- PYTHON/2D List.py
+++ PYTHON/2D List.py
@@ -3,5 +3,8 @@ matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
-matrix[0][1] = 100
-print(matrix[0][1])
+# matrix[0][1] = 100
+# print(matrix[0][1])
+for row in matrix:
+    for item in row:
+        print(item)
