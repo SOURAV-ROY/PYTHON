e6d41e7f31045bcf30e59b2e594fba082892cb98
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-23 01:23:09 +0600

    List All Methods

---
+++ PYTHON/ListMethod.py
@@ -0,0 +1,23 @@
+numbers = [10, 2, 3, 3, 4, 4, 5, 7]
+# numbers.append(30)
+# numbers.insert(1, 10)
+# numbers.remove(5)
+# numbers.clear()
+# numbers.pop()
+# print(numbers.index(4))
+# print(50 in numbers)  # False***********************
+# print(numbers.count(4))
+# numbers.sort()
+# numbers.reverse()
+# numbers2 = numbers.copy()
+# numbers.append(50)
+# print(numbers)
+# print(numbers2)
+
+# REMOVE DUPLICATE ITEMS ***********************
+uniqueList = []
+for number in numbers:
+    if number not in uniqueList:
+        uniqueList.append(number)
+print(uniqueList)
+
