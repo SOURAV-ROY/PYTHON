7fb2aa931fc2306cdb72ea2e83309dd7c1749d84
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-22 13:48:59 +0600

    CHEAT Also Correct'

--- PYTHON/NestedLoop.py
+++ PYTHON/NestedLoop.py
@@ -1,3 +1,7 @@
-for x in range(4):
-    for y in  range(3):
-        print(f"({x}, {y})")
+# for x in range(4):
+#     for y in range(4):
+#         print(f"({x}, {y})")
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    print("X"*x_count)   #CHEAT Also Correct ****************
