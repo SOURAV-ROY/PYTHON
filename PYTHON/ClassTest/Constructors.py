67cf421e6861701514f9f31a08aede539fbddae3
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-26 00:08:34 +0600

    Constructors Done

---
+++ PYTHON/ClassTest/Constructors.py
@@ -0,0 +1,23 @@
+class Point:
+    # def __init__(self, x, y):
+    #     self.x = x
+    #     self.y = y
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+
+    def move(self):
+        print("Move")
+
+    def draw(self):
+        print("Draw")
+
+
+# point = Point(10, 20)
+# point.x = 11
+# print(point.x)
+
+john = Point("Sourav Roy")
+# print(john.name)
+john.talk()
