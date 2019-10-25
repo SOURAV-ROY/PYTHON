9c0b7f2145ba1ae237e0f6b3a44742f3ab193b7f
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-26 00:27:29 +0600

    Inheritance Update

--- PYTHON/ClassTest/Inheritance.py
+++ PYTHON/ClassTest/Inheritance.py
@@ -2,15 +2,25 @@ class Animal():
     def walk(self):
         print("Walk")
 
+
 class Dog(Animal):
     pass
     # def bark(self):
     #     print("Bark")
 
+
 class Cat(Animal):
-    pass
+    # pass
+    def sound(self):
+        print("MEU MEU")
+
 
 dog1 = Dog()
+print("*****DOG*****")
 dog1.walk()
 # dog1.bark()
 
+cat1 = Cat()
+print("*****CAT*****")
+cat1.walk()
+cat1.sound()
