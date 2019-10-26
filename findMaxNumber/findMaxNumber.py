2dc15f73321d3be4ef53af0c184a4a4942430ef4
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-27 00:49:02 +0600

    Find Max Nunber By Function

---
+++ findMaxNumber/findMaxNumber.py
@@ -0,0 +1,6 @@
+# import utils
+from utils import findMaxNumber
+
+numbers = [1, 2, 3, 9, 4, 5, 10]
+max = findMaxNumber(numbers)
+print(max)
---
+++ findMaxNumber/utils.py
@@ -0,0 +1,7 @@
+def findMaxNumber(numbers):
+    max = numbers[0]
+    for number in numbers:
+        if number > max:
+            max = number
+    return max
+
