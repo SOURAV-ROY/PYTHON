f3b59f295ff321183d2b950806fd5d24f6cba27f
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-29 00:42:40 +0600

    Save Data With BarChart

--- xl/appXL.py
+++ xl/appXL.py
@@ -1,5 +1,7 @@
 # import openpyxl
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
 wb = xl.load_workbook("sourav.xlsx")
 sheet = wb['Sheet1']
 cell = sheet['a1']
@@ -16,4 +18,14 @@ for row in range(2, sheet.max_row + 1):
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
 
+values = Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row,
+          min_col=5,
+          max_col=5)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'f2')
+# wb.save('sourav.xlsx')
 wb.save('save.xlsx')
