63a953a57c937010733dc4013f1b0c15da65b165
SOURAV ROY <souravroy.info13@gmail.com>
2019-10-29 00:11:04 +0600

    Save In XL File

--- xl/appXL.py
+++ xl/appXL.py
@@ -9,6 +9,11 @@ cell = sheet.cell(1, 1)
 # print(sheet.max_row)
 # for row in range(1, sheet.max_row + 1):
 #       print(row)
-for row in range(1, sheet.max_row + 1):
+for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)  # 3 Is Column Number ****************
-    print(f"Price Column Value {[row]} = {cell.value}")
+    # print(f"Price Column Value {[row]} = {cell.value}")
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('save.xlsx')
